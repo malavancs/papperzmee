cc288fffcfd60f86c80bb28a876cd7d14156d7a4
malavancs <malavanhari3@gmail.com>
2019-12-22 22:58:17 +0530

    Navigation bar complete

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,8 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
 
   constructor() { }
-
+  options;
+  initializeUser() {
+    this.options = [{
+      class: 'fa fa-dashboard',
+      text: 'Dashboard'
+    }, {
+      class: 'fa fa-camer',
+      text: 'Hire a photographer'
+    }, {
+      class: 'fa fa-subscribe',
+      text: 'Subscribers'
+    }];
+  }
+  initializeAdmin() {
+    this.options = [{
+      class: 'fa fa-dashboard',
+      text: 'Dashboard'
+    }, {
+      class: 'fa fa-camera',
+      text: 'Photographers'
+    }, {
+      class: 'fa fa-address-book-o',
+      text: 'Subscribers'
+    }];
+  }
   ngOnInit() {
+
+    this.initializeAdmin();
+
   }
 
 }
