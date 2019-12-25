87433f193bb78428651df8a0590b0415cdfa6a32
malavancs <malavanhari3@gmail.com>
2019-12-25 08:45:31 +0530

    heroku issue fixed

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT);
